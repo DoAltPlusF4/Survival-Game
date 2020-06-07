b6270f2f47db7f40ab120ed54644400a919142a9
Lenny P Critchley <doaltplusf4@gmail.com>
2020-06-07 12:08:04 +0100

    Added **EPIC** entity class.

--- source/__init__.py
+++ source/__init__.py
@@ -1,2 +1,3 @@
+from . import camera, chunk, constants, entity
 from .dict_to_collider import dict_to_collider
 from .spiral import spiral
---
+++ source/entity.py
@@ -0,0 +1,54 @@
+import pyglet
+import pymunk
+
+from .dict_to_collider import dict_to_collider
+
+
+class Entity(pymunk.Body):
+    _space = None
+
+    def __init__(self, position=(0, 0), body_type=pymunk.Body.DYNAMIC, colliders: list = None):
+        super().__init__(mass=1, moment=float("inf"), body_type=body_type)
+        self.position = position
+        self.colliders = []
+
+        if colliders is not None:
+            for collider in colliders:
+                self.add_collider(colliders)
+
+    @property
+    def space(self):
+        return self._space
+
+    @space.setter
+    def space(self, space):
+        if self.space is not None:
+            self.space.remove(self, *self.colliders)
+
+        self._space = space
+        self.space.add(self, *self.colliders)
+
+    def add_collider(self, collider):
+        col = dict_to_collider(collider)
+        col.body = self
+
+        if self.space is not None:
+            self.space.add(col)
+
+        self.colliders.append(col)
+
+    def create_sprite(self, application, colour, batch=None, group=None):
+        self.sprite = pyglet.shapes.Rectangle(
+            x=self.position.x,
+            y=self.position.y,
+            width=14, height=14,
+            color=colour,
+            batch=batch,
+            group=group
+        )
+
+    def update_sprite(self):
+        self.sprite.position = tuple(self.position)
+
+    def delete(self):
+        self.sprite.delete()
